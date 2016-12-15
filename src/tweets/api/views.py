01b409f9d9b6e055225876fee15e8bc4b2788b85
Pablo Moreno Tepichin <pmoretepi@gmail.com>
2016-12-15 16:52:43 -0600

    Se implementa lista de tweets con ajax  y se implementa busqueda con jquery y ajax

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-
+from django.db.models import Q
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
@@ -7,5 +7,15 @@ from .serializers import TweetModelSerializer
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
-    def get_queryset(self):
-        return Tweet.objects.all()
+    # funcion para poder ejecutar una busqueda
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+            )
+        print(query)
+        return qs
